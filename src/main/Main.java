3f05921bef304ab26085b186c66b2203569efd4b
Marc Hvenegaard <mhvenegaard90@gmail.com>
2013-05-01 21:42:05 +0200

    Calendar

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package pack;
+package main;
 
 import java.sql.SQLException;
 import view.Loginframe;
