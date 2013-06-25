30c197aa5adf208c307c0626037dfaf80132ad68
kasper Franz <kasper@franzmedia.dk>
2013-06-25 08:54:51 +0200

    SKS Start

---
+++ src/model/SKS.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Kasper
+ */
+public class SKS {
+    ArrayList<SKS_Headline> headlines;
+    String installation;
+    Date installationDate;
+    Employee employee;
+
+    public SKS(ArrayList<SKS_Headline> headlines, String installation, Date installationDate, Employee employee) {
+        this.headlines = headlines;
+        this.installation = installation;
+        this.installationDate = installationDate;
+        this.employee = employee;
+    }
+
+    public SKS(ArrayList<SKS_Headline> headlines, String installation, Employee employee) {
+        this.headlines = headlines;
+        this.installation = installation;
+        this.employee = employee;
+    }
+
+
+    
+    
+}
