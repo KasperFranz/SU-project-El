c72470b1a6239a17dceb913cb82a1790be7e57fd
kasper Franz <kasper@franzmedia.dk>
2013-06-25 08:59:01 +0200

    headlines

--- src/model/SKS_Headline.java
+++ src/model/SKS_Headline.java
@@ -4,10 +4,37 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Kasper
  */
 public class SKS_Headline {
+    private ArrayList<SKS_question> questions;
+    private String headline;
+
+    public SKS_Headline(ArrayList<SKS_question> questions, String headline) {
+        this.questions = questions;
+        this.headline = headline;
+    }
+
+    public SKS_Headline(String headline) {
+        this.headline = headline;
+        questions = new ArrayList<>();
+    }
+
+    public ArrayList<SKS_question> getQuestions() {
+        return questions;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+    
+    
+    public void addQuestion(SKS_question question){
+        questions.add(question);
+    }
     
 }
